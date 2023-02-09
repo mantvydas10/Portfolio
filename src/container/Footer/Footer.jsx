8e06677d2955731e806db93bb2f15fe8d5bfdfa4
Mantvydas Andreikevicius <mandreikevicius@gmail.com>
2023-02-09 15:04:15 +0200

    implement mail validation

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -11,33 +11,73 @@ const Footer = () => {
     email: "",
     message: "",
   });
+  const [formError, setFormError] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { username, email, message } = formData;
+  const { name, email, message } = formData;
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    if (formError) {
+      setFormError("");
+    }
   };
 
-  const handleSubmit = () => {
-    setLoading(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const contact = {
-      _type: "contact",
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
+    let formIsValid = true;
+    const formDataCopy = { ...formData };
+    const formErrorCopy = { ...formError };
 
-    client
-      .create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
+    if (!formDataCopy.name.trim()) {
+      formIsValid = false;
+      formErrorCopy.name = "Name is required";
+    }
+
+    if (!formDataCopy.email.trim()) {
+      formIsValid = false;
+      formErrorCopy.email = "Email is required";
+    } else if (
+      !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+        formDataCopy.email
+      )
+    ) {
+      formIsValid = false;
+      formErrorCopy.email = "Email is not valid";
+    }
+
+    if (!formDataCopy.message.trim()) {
+      formIsValid = false;
+      formErrorCopy.message = "Message is required";
+    }
+
+    if (formIsValid) {
+      setLoading(true);
+
+      const contact = {
+        _type: "contact",
+        name: formData.name,
+        email: formData.email,
+        message: formData.message,
+      };
+
+      client
+        .create(contact)
+        .then(() => {
+          setLoading(false);
+          setIsFormSubmitted(true);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setFormError(formErrorCopy);
+    }
   };
 
   return (
@@ -65,10 +105,11 @@ const Footer = () => {
               className="p-text"
               type="text"
               placeholder="Your Name"
-              name="username"
-              value={username}
+              name="name"
+              value={name}
               onChange={handleChangeInput}
             />
+            {formError.name && <p className="error">{formError.name}</p>}
           </div>
           <div className="app__flex">
             <input
@@ -79,6 +120,7 @@ const Footer = () => {
               value={email}
               onChange={handleChangeInput}
             />
+            {formError.email && <p className="error">{formError.email}</p>}
           </div>
           <div>
             <textarea
@@ -88,6 +130,7 @@ const Footer = () => {
               name="message"
               onChange={handleChangeInput}
             />
+            {formError.message && <p className="error">{formError.message}</p>}
           </div>
           <button type="button" className="p-text" onClick={handleSubmit}>
             {!loading ? "Send Message" : "Sending..."}
